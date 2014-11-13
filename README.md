2e93aeb76171253416ebdb5579788df15d3483f6
andrewelizondo <elizondo_andre@live.com>
2014-11-12 17:07:14 -0800

    styling, readme updated

--- README.md
+++ README.md
@@ -1,7 +1,60 @@
 # chef-provisioning-winrm
 
-Partially working but still a WIP, create action works correctly however destroy will not delete the node object.
+Partially working but still a WIP, currently able to bootstrap machines over winrm using http.
 
-Note Windows with Chef provisioning is currently only usable with a hosted/on-premise chef server, chef-zero will not work here. :(
+**Note:** Windows with Chef provisioning is currently only usable with a hosted/on-premise chef server, chef-zero will not work here. :(
 
-May blow shit up, may not.
+## Installation
+
+Clone this repository.
+
+	$ git clone https://github.com/andrewelizondo/chef-provisioning-winrm.git
+
+Build the gem.
+
+	$ gem build chef-provisioning-winrm.gemspec
+
+**Note** If you're using ChefDK, this should be installed under the ChefDK Ruby using `chef gem <build|install>`
+
+
+Install the gem.
+
+	$ sudo gem install chef-provisioning-winrm-0.1.1.gem
+
+## Usage
+
+* required machine options: only one is required
+
+  :ip\_address,
+  :fqdn
+
+* valid options:
+
+	:winrm\_user, - defaults to Administrator
+	:winrm\_pass, - defaults to an empty string
+	:winrm\_port, - defaults to 5985
+
+## Example
+
+```ruby
+
+		require 'chef/provisioning/winrm_driver'
+
+		with_driver 'winrm'
+
+		with_chef_server 'https://api.opscode.com/organizations/<your_org>',{
+			:client_name => Chef::Config[:node_name],
+			:signing_key_filename => Chef::Config[:client_key]
+		}
+
+		machine 'what-do' do
+			action [:converge]
+			machine_options :winrm_options => {
+				:winrm_user => 'vagrant',
+				:winrm_pass => 'vagrant',
+				:winrm_port => '5985',
+				:ip_address => '192.168.243.198'
+			}
+			role 'win_base'
+		end
+```
--- lib/chef/provisioning/winrm_driver/driver.rb
+++ lib/chef/provisioning/winrm_driver/driver.rb
@@ -7,131 +7,137 @@ require 'chef/provisioning/winrm_driver/version'
 require 'resolv'
 
 class Chef
-	module Provisioning
-		module WinRMDriver
-			# Provision machines over WinRM
-			class Driver < Chef::Provisioning::Driver
+  module Provisioning
+    module WinRMDriver
+      # Provision machines over WinRM
+      class Driver < Chef::Provisioning::Driver
 
-				def initialize(driver_url, config)
-					super
-				end
+        def initialize(driver_url, config)
+          super
+        end
 
-				attr_reader :connection
+        attr_reader :connection
 
-				def self.from_url(driver_url, config)
-					Driver.new(driver_url, config)
-				end
+        def self.from_url(driver_url, config)
+          Driver.new(driver_url, config)
+        end
 
-				def self.canonicalize_url(driver_url, config)
-					url = driver_url.split(':')[1]
-					[ driver_url, config ]
-				end
+        def self.canonicalize_url(driver_url, config)
+          # still not exactly sure what's happening here, but it works
+          url = driver_url.split(':')[1]
+          [ driver_url, config ]
+        end
 
-				def allocate_machine(action_handler, machine_spec, machine_options)
-					target_name = machine_spec.name
+        def allocate_machine(action_handler, machine_spec, machine_options)
+          target_name = machine_spec.name
 
-					winrm_options = machine_options[:winrm_options]
+          winrm_options = validate_options(machine_options[:winrm_options])
 
-					validate_options(winrm_options)
+          if !winrm_options[:ip_address].nil?
+            @remote = valid_ip?(winrm_options[:ip_address])
+          elsif !winrm_options[:fqdn].nil?
+            @remote = valid_target?(winrm_options[:fqdn])
+          else
+            Chef::Log.fatal("No valid method of reaching this host!")
+          end
+          
+          Chef::Log.debug("Using #{@remote}")
 
-					if !winrm_options[:ip_address].nil?
-						@remote = valid_ip?(winrm_options[:ip_address])
-					elsif !winrm_options[:fqdn].nil?
-						@remote = valid_target?(winrm_options[:fqdn])
-					else
-						Chef::Log.fatal("=> No valid method of reaching this host!")
-					end
-					
-					Chef::Log.debug("=> Using #{@remote}")
+          @endpoint = winrm_endpoint(@remote, winrm_options[:winrm_port])
 
-					@endpoint = winrm_endpoint(@remote)
+          # no actual allocation needed here, just need to tell the chef server stuffs
+          if !machine_spec.location
+            machine_spec.location = {
+              'driver_url' => driver_url,
+              'driver_version' => Chef::Provisioning::WinRMDriver::VERSION,
+              'target_name' => target_name,
+              'target_endpoint' => @endpoint,
+              'allocated_at' => Time.now.utc.to_s
+            }
+          end
 
-					# no actual allocation needed here, just need to tell the chef server stuffs
-					if !machine_spec.location
-						machine_spec.location = {
-							'driver_url' => driver_url,
-							'driver_version' => Chef::Provisioning::WinRMDriver::VERSION,
-							'target_name' => target_name,
-							'target_endpoint' => @endpoint,
-							'allocated_at' => Time.now.utc.to_s
-						}
-					end
+        end
 
-				end
+        def ready_machine(action_handler, machine_spec, machine_options)
+          machine_for(machine_spec, machine_options)
+        end
 
-				def ready_machine(action_handler, machine_spec, machine_options)
-					machine_for(machine_spec, machine_options)
-				end
+        def machine_for(machine_spec, machine_options)
+          winrm_options = machine_options[:winrm_options]
+          options = {
+            :user => winrm_options[:winrm_user],
+            :pass => winrm_options[:winrm_pass],
+            :disable_sspi => true,
+            :basic_auth_only => true
+          }
+          type = :plaintext
+          transport = Chef::Provisioning::Transport::WinRM.new(@endpoint, type, options, config)
+          convergence_strategy = convergence_for(machine_options[:convergence_options], config)
+          Chef::Provisioning::Machine::WindowsMachine.new(machine_spec, transport, convergence_strategy)
+        end
 
-				def machine_for(machine_spec, machine_options)
-					winrm_options = machine_options[:winrm_options]
-					options = {
-						:user => winrm_options[:winrm_user],
-						:pass => winrm_options[:winrm_pass],
-						:disable_sspi => true,
-						:basic_auth_only => true
-					}
-					type = :plaintext
-					transport = Chef::Provisioning::Transport::WinRM.new(@endpoint, type, options, config)
-					convergence_strategy = convergence_for(machine_options[:convergence_options], config)
-					Chef::Provisioning::Machine::WindowsMachine.new(machine_spec, transport, convergence_strategy)
-				end
+        def destroy_machine(action_handler, machine_spec, machine_options)
+          action_handler.perform_action "Removing machine #{machine_spec.name}" do
+            machine_spec.location = nil
+          end
+          strategy = convergence_for(machine_options[:convergence_options],config)
+          strategy.cleanup_convergence(action_handler, machine_spec)
+          Chef::Log.debug("#{machine_spec.name} removed from the Chef server")
+        end
 
-				def destroy_machine(action_handler, machine_spec, machine_options)
-					action_handler.perform_action "=> Removing machine #{machine_spec.name}" do
-						machine_spec.location = nil
-					end
-					strategy = convergence_for(machine_options[:convergence_options],config)
-					strategy.cleanup_convergence(action_handler, machine_spec)
-					Chef::Log.debug("=> #{machine_spec.name} removed from the Chef server")
-				end
+        def stop_machine(action_handler, machine_spec, machine_options)
+          Chef::Log.warn("Machine action :stop is unsupported")
+        end
 
-				def stop_machine(action_handler, machine_spec, machine_options)
-					Chef::Log.warn("=>! Machine action :stop is unsupported")
-				end
+        def connect_to_machine(machine_spec, machine_options)
+          machine_for(machine_spec, machine_options)
+        end
 
-				def connect_to_machine(machine_spec, machine_options)
-					machine_for(machine_spec, machine_options)
-				end
+        def validate_options(machine_options)
+          valid_machine_options = %w{
+            ip_address
+            fqdn
+            winrm_port
+            timeout
+            winrm_user
+            winrm_pass
+          }
+          machine_options.each { |k,v| Chef::Log.fatal("Invalid machine option! #{k}") unless valid_machine_options.include?(k.to_s) }
+          
+          # set some defaults
+          machine_options[:winrm_user] ||= 'Administrator'
+          machine_options[:winrm_pass] ||= ''
+          machine_options[:winrm_port] ||= 5985
 
-				def validate_options(machine_options)
-					valid_machine_options = %w{
-						ip_address
-						fqdn
-						winrm_port
-						timeout
-						winrm_user
-						winrm_pass
-					}
-					machine_options.each { |k,v| Chef::Log.fatal("Invalid machine option! #{k}") unless valid_machine_options.include?(k.to_s) }
-				end
+          machine_options
+        end
 
-				def valid_ip?(address)
-					!!Resolv::AddressRegex.match(address) || Chef::Log.fatal("=>! You've given me an invalid IP address! #{address}")
-					address
-				end
+        def valid_ip?(address)
+          !!Resolv::AddressRegex.match(address) || Chef::Log.fatal("You've given me an invalid IP address! #{address}")
+          address
+        end
 
-				def valid_target?(fqdn)
-					begin
-						Resolv.getaddress(fqdn)
-					rescue
-						Chef::Log.fatal("=>! Unable to resolve target! #{fqdn}")
-					end
-				end
+        def valid_target?(fqdn)
+          begin
+            Resolv.getaddress(fqdn)
+          rescue
+            Chef::Log.fatal("Unable to resolve target! #{fqdn}")
+          end
+        end
 
-				def winrm_endpoint(fqdn, port=5985)
-					endpoint = "http://#{fqdn}:#{port}/wsman"
-					Chef::Log.debug("=> Using #{endpoint} endpoint")
-					endpoint
-				end
+        def winrm_endpoint(fqdn, port)
+          endpoint = "http://#{fqdn}:#{port}/wsman"
+          Chef::Log.debug("Using #{endpoint} endpoint")
+          endpoint
+        end
 
-				def convergence_for(convergence_options, config)
-					Chef::Provisioning::ConvergenceStrategy::InstallMsi.new(convergence_options, config)
-				end
+        def convergence_for(convergence_options, config)
+          Chef::Provisioning::ConvergenceStrategy::InstallMsi.new(convergence_options, config)
+        end
 
-			end
-		end
-	end
+      end
+    end
+  end
 end
 
 
